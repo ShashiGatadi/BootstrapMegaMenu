a7560d578953d020b348e6a5d42944ed8b255567
Shashi Gatadi <shashi.gatadi@outlook.com>
2018-02-28 15:24:39 -0800

    imported project

---
+++ BootstrapMegaMenu/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BootstrapMegaMenu.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
